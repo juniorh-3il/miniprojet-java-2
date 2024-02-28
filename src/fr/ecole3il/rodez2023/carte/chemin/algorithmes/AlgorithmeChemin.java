313a65c2d6f54230fce0aa05763989b1595fd626
juniorh-3il <juniorh@3il.fr>
2024-02-28 11:22:08 +0100

    🚑️ Convertion de la classe AlgorithmeChemin en interface

--- src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeChemin.java
+++ src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeChemin.java
@@ -1,4 +1,10 @@
 package fr.ecole3il.rodez2023.carte.chemin.algorithmes;
 
-public class AlgorithmeChemin {
+import fr.ecole3il.rodez2023.carte.elements.Noeud;
+import fr.ecole3il.rodez2023.carte.elements.Graphe;
+
+import java.util.List;
+
+public interface AlgorithmeChemin<E> {
+    List<Noeud<E>> trouverChemin(Graphe<E> graphe, Noeud<E> depart, Noeud<E> arrivee);
 }
