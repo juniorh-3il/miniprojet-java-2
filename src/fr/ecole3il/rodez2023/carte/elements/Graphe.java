50c1023924181d93bcc38a310d9113b870df24cf
hjunior <hjunior@pros.com>
2024-03-01 09:55:44 +0100

    💡 Rédaction de Javadocs pour les classes Graphe Graphe et Noeud

--- src/fr/ecole3il/rodez2023/carte/elements/Graphe.java
+++ src/fr/ecole3il/rodez2023/carte/elements/Graphe.java
@@ -10,15 +10,31 @@ public class Graphe<E> {
     private final List<Noeud<E>> noeuds;
     private final Map<Integer, Map<Integer, Double>> aretes;
 
+
     public Graphe() {
         this.noeuds = new ArrayList<Noeud<E>>();
         this.aretes = new HashMap<Integer, Map<Integer, Double>>();
     }
 
+    /**
+     * Ajoute un nœud au graphe si le nœud n'existe pas déjà dans le graphe.
+     * @param noeud Nœud à ajouter
+     */
     public void ajouterNoeud(Noeud<E> noeud) {
-        this.noeuds.add(noeud);
+        if (!this.noeuds.contains(noeud)) {
+            this.noeuds.add(noeud);
+        }
     }
 
+    /**
+     * Ajoute une arête pondérée entre deux nœuds du graphe.
+     * Elle prend en paramètre le nœud de départ, le nœud d'arrivée et le coût de l'arête.
+     * Si les nœuds de départ et d'arrivée n'existent pas encore dans le graphe,
+     * ils sont ajoutés en utilisant la méthode <code>ajouterNoeud()</code>.
+     * @param depart Nœud de départ de l'arête
+     * @param arrivee Nœud d'arrivée de l'arête
+     * @param cout Coût associé à l'arête
+     */
     public void ajouterArete(Noeud<E> depart, Noeud<E> arrivee, double cout) {
         if (!this.noeuds.contains(depart)) {
             this.ajouterNoeud(depart);
@@ -37,6 +53,13 @@ public class Graphe<E> {
         arrivee.ajouterVoisin(depart);
     }
 
+    /**
+     *  Renvoie le coût de l'arête entre deux nœuds spécifiés.
+     * @param depart Nœud de départ de l'arête dont on cherche le coût
+     * @param arrivee Nœud de départ de l'arête dont on cherche le coût
+     * @return Coût de l'arête
+     * @throws Exception Le nœud de départ ou d'arrivée spécifié n'existe pas dans le graphe
+     */
     public Double getCoutArete(Noeud<E> depart, Noeud<E> arrivee) throws Exception {
         if (!this.noeuds.contains(depart)) {
             throw new Exception("Noeud de départ absent du graphe");
@@ -55,19 +78,25 @@ public class Graphe<E> {
         return this.noeuds;
     }
 
-    public List<Noeud<E>> getVoisins(Noeud<E> noeud) throws Exception {
-        if (!this.noeuds.contains(noeud)) {
-            throw new Exception("Noeud absent");
-        }
-
+    /**
+     * Renvoie une liste contenant tous les voisins d'un nœud spécifié.
+     * Si le nœud n'existe pas dans le graphe, elle renvoie une liste vide.
+     * @param noeud Nœud pour lequel on cherche les voisins
+     * @return Les voisins du nœud spécifié en paramètre
+     */
+    public List<Noeud<E>> getVoisins(Noeud<E> noeud) {
         List<Noeud<E>> voisins = new ArrayList<Noeud<E>>();
-
-        for (Noeud<E> eventuelVoisin : this.noeuds) {
-            if (this.getCoutArete(noeud, eventuelVoisin) != null) {
-                voisins.add(eventuelVoisin);
+        if (this.noeuds.contains(noeud)) {
+            for (Noeud<E> eventuelVoisin : this.noeuds) {
+                try {
+                    if (this.getCoutArete(noeud, eventuelVoisin) != null) {
+                        voisins.add(eventuelVoisin);
+                    }
+                } catch (Exception e) {
+                    //TODO
+                }
             }
         }
-
         return voisins;
     }
 }
--- src/fr/ecole3il/rodez2023/carte/elements/Noeud.java
+++ src/fr/ecole3il/rodez2023/carte/elements/Noeud.java
@@ -8,9 +8,21 @@ public class Noeud<E> {
     private E valeur;
     private List<Noeud<E>> voisins;
 
+    /**
+     * Ce constructeur crée un nouvel objet Noeud avec la valeur spécifiée et initialise la liste de voisins.
+     * @param valeur La valeur attribuée au noeud
+     */
     public Noeud(E valeur) {
         this.valeur = valeur;
         this.voisins = new ArrayList<Noeud<E>>();
+    }
+
+    /**
+     * Ajouter un nœud voisin à la liste des voisins du nœud actuel.
+     * @param voisin Voisin à ajouter
+     */
+    public void ajouterVoisin(Noeud<E> voisin) {
+        this.voisins.add(voisin);
     }
 
     public E getValeur() {
@@ -19,9 +31,5 @@ public class Noeud<E> {
 
     public List<Noeud<E>> getVoisins() {
         return this.voisins;
-    }
-
-    public void ajouterVoisin(Noeud<E> voisin) {
-        this.voisins.add(voisin);
     }
 }
